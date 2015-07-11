065dfe027a3c27b1aad30da38410aa9852b01ac3
liuzxc <lxq_102172@163.com>
2015-07-11 11:41:13 +0800

    improve code

--- google_currency_convert.gemspec
+++ google_currency_convert.gemspec
@@ -1,12 +1,13 @@
 Gem::Specification.new do |s|
   s.name        = 'google_currency_convert'
-  s.version     = '0.0.1'
+  s.version     = '0.0.2'
   s.date        = '2015-07-09'
   s.summary     = "Google currency convert"
   s.description = "Google currency convert by google finance API"
   s.authors     = ["liuxingqi"]
   s.email       = 'lxq_102172@163.com'
-  s.files       = ["lib/google_currency_convert.rb", "lib/curr_code.csv"]
+  s.files       = ["lib/google_currency_convert.rb", "lib/curr_code.csv",
+                   "test/google_currency_convert_spec.rb", "README.md"]
   s.homepage    = 'http://rubygems.org/gems/google_currency_convert'
   s.license       = 'MIT'
 end
--- lib/google_currency_convert.rb
+++ lib/google_currency_convert.rb
@@ -8,8 +8,8 @@ class GoogleCurrencyConvert
 			@info = []
 			file_path = File.expand_path("curr_code.csv", File.dirname(__FILE__))
 			IO.readlines(file_path).each { |line| @info << line.strip }
-	    end
-	    @info
+	  end
+	  @info
 	end
 
 	def self.currency_codes
@@ -24,14 +24,14 @@ class GoogleCurrencyConvert
 
 		return "Error: amount must be a positive integer" if amount <= 0 or amount.class != Fixnum
 
-	    host = "www.google.com"
-	    http = Net::HTTP.new(host, 80)
-	    url = "/finance/converter?a=#{amount}&from=#{from}&to=#{to}"
-	    response = http.get(url)
+    host = "www.google.com"
+    http = Net::HTTP.new(host, 80)
+    url = "/finance/converter?a=#{amount}&from=#{from}&to=#{to}"
+    response = http.get(url)
 
-	    doc = Nokogiri::HTML(response.body)
-	    result = doc.css('div#currency_converter_result').first.text
-	    result.split(' ')[3].to_f
+    doc = Nokogiri::HTML(response.body)
+    result = doc.css('div#currency_converter_result').first.text
+    result.split(' ')[3].to_f
 	end
 
 end
