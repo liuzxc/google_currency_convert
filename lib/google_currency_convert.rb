7e61bd02106cb509d43171da1c5b3d72fbf954b9
liuzxc <lxq_102172@163.com>
2015-07-16 10:50:11 +0800

    improve codes

--- lib/google_currency_convert.rb
+++ lib/google_currency_convert.rb
@@ -3,28 +3,28 @@ require_relative './http_method.rb'
 
 class GoogleCurrencyConvert
 
-	extend HttpMethod
+  extend HttpMethod
 
-	URL = "https://www.google.com/finance/converter"
+  URL = "https://www.google.com/finance/converter"
 
-	def self.currency_info
-		unless @info
-			@info = []
-			file_path = File.expand_path("curr_code.csv", File.dirname(__FILE__))
-			IO.readlines(file_path).each { |line| @info << line.strip }
-	  end
-	  @info
-	end
+  def self.currency_info
+    unless @info
+      @info = []
+      file_path = File.expand_path("curr_code.csv", File.dirname(__FILE__))
+      IO.readlines(file_path).each { |line| @info << line.strip }
+    end
+    @info
+  end
 
-	def self.currency_codes
-		(@info ||= currency_info).map { |i| i.split(' ')[0] }
-	end
+  def self.currency_codes
+    (@info ||= currency_info).map { |i| i.split(' ')[0] }
+  end
 
-	def self.currency_convert(from, to, amount = 1)
-		if not currency_codes.include?(from) or not currency_codes.include?(to)
-			return "Error: cannot support such currency code"
-		end
-		return "Error: amount must be a positive integer" if amount <= 0 or amount.class != Fixnum
+  def self.currency_convert(from, to, amount = 1)
+    if not currency_codes.include?(from) or not currency_codes.include?(to)
+      return "Error: cannot support such currency code"
+    end
+    return "Error: amount must be a positive integer" if amount <= 0 or amount.class != Fixnum
 
     params = {from: from, to: to, a: amount}
     response = https_get(URL, params)
@@ -33,6 +33,6 @@ class GoogleCurrencyConvert
     doc = Nokogiri::HTML(response.body)
     result = doc.css('div#currency_converter_result').first.text
     result.split(' ')[3].to_f
-	end
+  end
 
 end
--- lib/http_method.rb
+++ lib/http_method.rb
@@ -2,13 +2,13 @@ require 'net/http'
 
 module HttpMethod
 
-	def https_get(url, params)
-		uri = URI(url)
-		uri.query = URI.encode_www_form(params)
-		http = Net::HTTP.new(uri.host, uri.port)
+  def https_get(url, params)
+    uri = URI(url)
+    uri.query = URI.encode_www_form(params)
+    http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
     request = Net::HTTP::Get.new(uri.request_uri)
     response = http.request(request)
-	end
+  end
 
 end
